d213b5e7c208529e07d6289c5b2f8dabb12c500a
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2015-09-28 16:53:40 -0600

    Refactor: Updated Tests
    Updates tests for ArrayGrid and the map

--- src/test/java/com/csm/rover/simulator/test/Incline.java
+++ src/test/java/com/csm/rover/simulator/test/Incline.java
@@ -1,15 +1,15 @@
 package com.csm.rover.simulator.test;
 
-import static org.junit.Assert.*;
+import com.csm.rover.simulator.map.display.LandMapPanel;
+import com.csm.rover.simulator.objects.DecimalPoint;
+import com.csm.rover.simulator.objects.FloatArrayArrayGrid;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.awt.Dimension;
 import java.awt.Point;
 
-import com.csm.rover.simulator.map.display.LandMapPanel;
-import com.csm.rover.simulator.objects.DecimalPoint;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class Incline {
 
@@ -18,13 +18,12 @@ public class Incline {
 	@Before
 	public void set(){
 		map = new LandMapPanel(new Dimension(100, 100), null);
-		map.heightMap.setDetail(1);
-		map.heightMap.setValues(new float[][]{
-				{ 0, 0, 0, 0 },
-				{ 1, 1, 1, 1 },
-				{ 2, 2, 2, 2 },
-				{ 3, 3, 3, 3 }
-		});
+		map.heightMap.setValues(4, 1, new FloatArrayArrayGrid(new Float[][]{
+				{ 0f, 0f, 0f, 0f },
+				{ 1f, 1f, 1f, 1f },
+				{ 2f, 2f, 2f, 2f },
+				{ 3f, 3f, 3f, 3f }
+		}));
 	}
 	
 	@Test
@@ -38,15 +37,14 @@ public class Incline {
 	
 	@Test
 	public void rightSquare(){
-		map.heightMap.setDetail(3);
-		map.heightMap.setValues(new float[][]{
-				{ 0, 0.1f, 0.2f, 1, 1.1f, 1.2f },
+		map.heightMap.setValues(6, 3, new FloatArrayArrayGrid(new Float[][]{
+				{ 0f, 0.1f, 0.2f, 1f, 1.1f, 1.2f },
 				{ 0.01f, 0.11f, 0.21f, 1.01f, 1.11f, 1.21f },
 				{ 0.02f, 0.12f, 0.22f, 1.02f, 1.12f, 1.22f },
-				{ 4, 4.1f, 4.2f, 5, 5.1f, 5.2f },
+				{ 4f, 4.1f, 4.2f, 5f, 5.1f, 5.2f },
 				{ 4.01f, 4.11f, 4.21f, 5.01f, 5.11f, 5.21f },
 				{ 4.02f, 4.12f, 4.22f, 5.02f, 5.12f, 5.22f }
-		});
+		}));
 		assertEquals(new Point(2, 3), map.heightMap.getMapSquare(new DecimalPoint(-.1, -.1)));
 		assertEquals(new Point(5, 5), map.heightMap.getMapSquare(new DecimalPoint(0.9, -0.9)));
 		assertEquals(new Point(1, 0), map.heightMap.getMapSquare(new DecimalPoint(-0.5, 0.7)));
--- src/test/java/com/csm/rover/simulator/test/objects/FloatArrayArrayGridTest.java
+++ src/test/java/com/csm/rover/simulator/test/objects/FloatArrayArrayGridTest.java
@@ -1,7 +1,7 @@
 package com.csm.rover.simulator.test.objects;
 
 import com.csm.rover.simulator.objects.ArrayGrid;
-import com.csm.rover.simulator.objects.GenericArrayGrid;
+import com.csm.rover.simulator.objects.FloatArrayArrayGrid;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,51 +9,51 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
-public class ArrayGridTest {
+public class FloatArrayArrayGridTest {
 
-    private ArrayGrid<String> grid;
+    private ArrayGrid<Float> grid;
 
     @Before
     public void setup(){
-        grid = new GenericArrayGrid<String>();
+        grid = new FloatArrayArrayGrid();
     }
 
     @Test
     public void testAddToBlank(){
-        grid.put(5, 4, "Hi");
+        grid.put(5, 4, 1.2f);
 
         assertEquals(6, grid.getWidth());
         assertEquals(5, grid.getHeight());
-        assertEquals("Hi", grid.get(5, 4));
+        assertEquals(1.2f, grid.get(5, 4), 0.001);
     }
 
     @Test
     public void testAddCol(){
-        ArrayList<String> col = new ArrayList<String>();
-        col.add("a");
-        col.add("b");
-        col.add("e");
-        col.add("f");
+        ArrayList<Float> col = new ArrayList<Float>();
+        col.add(1.4f);
+        col.add(1.5f);
+        col.add(1.6f);
+        col.add(1.7f);
         grid.addColumnAt(2, col);
 
         assertEquals(3, grid.getWidth());
         assertEquals(4, grid.getHeight());
-        assertEquals("e", grid.get(2, 2));
+        assertEquals(1.6f, grid.get(2, 2), 0.001);
     }
 
     @Test
     public void testAddRow(){
-        ArrayList<String> row = new ArrayList<String>();
-        row.add("a");
-        row.add("b");
-        row.add("c");
-        row.add("e");
-        row.add("f");
+        ArrayList<Float> row = new ArrayList<Float>();
+        row.add(1.4f);
+        row.add(1.5f);
+        row.add(1.6f);
+        row.add(1.7f);
+        row.add(1.8f);
         grid.addRowAt(5, row);
         System.out.println(grid);
         assertEquals(5, grid.getWidth());
         assertEquals(6, grid.getHeight());
-        assertEquals("b", grid.get(1, 5));
+        assertEquals(1.5f, grid.get(1, 5), 0.001);
     }
 
 //    @Test
