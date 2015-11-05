6e402a8bcb46990bcd6065582355e1226eebde66
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2015-11-05 16:46:11 -0700

    Object Class Created, Physics Updated
    Created object class and re-formatted all variables to fit sub. fixed
    some formatting issues with physics

---
+++ src/main/java/com/csm/rover/simulator/sub/subProp.java
@@ -0,0 +1,27 @@
+package com.csm.rover.simulator.sub;
+
+public enum subProp {
+
+    L(0),
+    LEFT(0),
+    R(1),
+    RIGHT(1),
+    F(2),
+    FRONT(2),
+    B(3),
+    BACK(3);
+
+    private int value;
+
+    subProp(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public boolean equals(subProp other){
+        return value == other.value;
+    }
+}
