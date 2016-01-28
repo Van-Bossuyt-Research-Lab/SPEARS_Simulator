8eb8bacb335f654d283455258b653effb02a593f
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2016-01-27 21:00:33 -0700

    Platform_Config: Platform Builder
    Implements a builder for the the PlatformConfig object

--- src/main/java/com/csm/rover/simulator/objects/io/PlatformConfig.java
+++ src/main/java/com/csm/rover/simulator/objects/io/PlatformConfig.java
@@ -5,11 +5,17 @@ import java.util.TreeMap;
 
 public class PlatformConfig {
 
-    private String type;
-    private String screenName;
+    String type;
+    String screenName;
 
-    private PhysicsConfig physics;
-    private AutonomousConfig autonomous;
+    PhysicsConfig physics;
+    AutonomousConfig autonomous;
+
+    PlatformConfig(){}
+
+    public static Builder builder(){
+        return new Builder();
+    }
 
     public String getType(){
         return type;
@@ -57,4 +63,92 @@ class AutonomousConfig {
 class Parameter {
     String name;
     double val;
+}
+
+class Builder {
+
+    private PlatformConfig config;
+
+    public Builder(){
+        config = new PlatformConfig();
+    }
+
+    public PlatformConfig build(){
+        String missing = "";
+        if (config.type == null){
+            missing += " type";
+        }
+        if (config.screenName == null){
+            missing += " screenName";
+        }
+        if (config.physics == null){
+            missing += " PhysicsModel";
+        }
+        if (config.autonomous == null) {
+            missing += " AutonomousModel";
+        }
+        if (!missing.equals("")){
+            throw new IllegalStateException("The PlatformConfiguration being built was not fully initialized.  Missing:" + missing);
+        }
+        return config;
+    }
+
+    public Builder setType(String type){
+        config.type = type;
+        return this;
+    }
+
+    public Builder setScreenName(String name){
+        config.screenName = name;
+        return this;
+    }
+
+    public Builder setPhysicsModel(String name){
+        return setPhysicsModel(name, null);
+    }
+
+    public Builder setPhysicsModel(String name, Map<String, Double> params){
+        PhysicsConfig physics = new PhysicsConfig();
+        physics.type = name;
+        if (params == null){
+            params = new TreeMap<String, Double>();
+        }
+        Parameter[] pars = new Parameter[params.size()];
+        int i = 0;
+        for (String key : params.keySet()){
+            Parameter par = new Parameter();
+            par.name = key;
+            par.val = params.get(key);
+            pars[i] = par;
+            i++;
+        }
+        physics.params = pars;
+        config.physics = physics;
+        return this;
+    }
+
+    public Builder setAutonomousModel(String name){
+        return setAutonomousModel(name, null);
+    }
+
+    public Builder setAutonomousModel(String name, Map<String, Double> params){
+        AutonomousConfig autonomous = new AutonomousConfig();
+        autonomous.type = name;
+        if (params == null){
+            params = new TreeMap<String, Double>();
+        }
+        Parameter[] pars = new Parameter[params.size()];
+        int i = 0;
+        for (String key : params.keySet()){
+            Parameter par = new Parameter();
+            par.name = key;
+            par.val = params.get(key);
+            pars[i] = par;
+            i++;
+        }
+        autonomous.params = pars;
+        config.autonomous = autonomous;
+        return this;
+    }
+
 }
