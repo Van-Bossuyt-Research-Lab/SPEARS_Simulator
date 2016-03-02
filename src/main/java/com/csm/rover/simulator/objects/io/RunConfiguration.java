22b671d53450d97dc1c00be644cc7706d4136753
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2016-03-01 21:22:00 -0700

    Json i/o with platforms
    RunConfig from file should support json files reading into platforms

--- src/main/java/com/csm/rover/simulator/objects/io/RunConfiguration.java
+++ src/main/java/com/csm/rover/simulator/objects/io/RunConfiguration.java
@@ -9,11 +9,13 @@ import com.csm.rover.simulator.platforms.rover.phsicsModels.RoverPhysicsModel;
 import com.csm.rover.simulator.platforms.satellite.SatelliteAutonomousCode;
 import com.csm.rover.simulator.platforms.satellite.SatelliteObject;
 import com.csm.rover.simulator.platforms.satellite.SatelliteParametersList;
+import com.csm.rover.simulator.platforms.satellite.SatelliteState;
 import com.csm.rover.simulator.wrapper.NamesAndTags;
 
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.*;
@@ -95,32 +97,10 @@ public class RunConfiguration implements Serializable {
 		while (jp.nextToken() != JsonToken.END_OBJECT) {
 			String fieldname = jp.getCurrentName();
 			jp.nextToken();
-			if("namesAndTags".equals(fieldname)){
 				while(jp.nextToken() != JsonToken.END_OBJECT){
 					String namefield = jp.getCurrentName();
 					jp.nextToken();
-					if("GroundNames".equals(namefield)){
-						NT.groundNames =  jp.readValueAs(ArrayList.class);
-					}
-					if("RoverNames".equals(namefield)){
-						NT.roverNames =  jp.readValueAs(ArrayList.class);
-					}
-					if("SatelliteNames".equals(namefield)){
-						NT.satelliteNames =  jp.readValueAs(ArrayList.class);
-					}
-					if("GroundTags".equals(namefield)){
-						NT.groundTags =  jp.readValueAs(ArrayList.class);
-					}
-					if("RoverTags".equals(namefield)){
-						NT.roverTags =  jp.readValueAs(ArrayList.class);
-					}
-					if("SatelliteTags".equals(namefield)){
-						NT.satelliteTags =  jp.readValueAs(ArrayList.class);
-					}
-					if("Runtime".equals(namefield)){
-						this.runtime = jp.readValueAs(int.class);
-						this.accelerated = jp.readValueAs(boolean.class);
-					}
+
 					if("Map".equals(namefield)){
 						mapFromFile = jp.readValueAs(boolean.class);
 						File fle = new File(jp.getValueAsString());
@@ -137,40 +117,85 @@ public class RunConfiguration implements Serializable {
 						targetDensity = jp.readValueAs(double.class);
 						hazardDensity = jp.readValueAs(double.class);
 					}
+					if("PlatformConfig".equals(namefield)){
+						PlatformConfig pc = new PlatformConfig();
+						Iterator<String> ID = new Iterator<String>() {
+							@Override
+							public boolean hasNext() {
+								return false;
+							}
+
+							@Override
+							public String next() {
+								return null;
+							}
+						};
+						Iterator<String> names = new Iterator<String>() {
+							@Override
+							public boolean hasNext() {
+								return false;
+							}
+
+							@Override
+							public String next() {
+								return null;
+							}
+						};
+						Iterator<String> types = new Iterator<String>() {
+							@Override
+							public boolean hasNext() {
+								return false;
+							}
+
+							@Override
+							public String next() {
+								return null;
+							}
+						};
+						Iterator<String> physics = new Iterator<String>() {
+							@Override
+							public boolean hasNext() {
+								return false;
+							}
+
+							@Override
+							public String next() {
+								return null;
+							}
+						};
+						Iterator<String> autos = new Iterator<String>() {
+							@Override
+							public boolean hasNext() {
+								return false;
+							}
+
+							@Override
+							public String next() {
+								return null;
+							}
+						};
+						ID = jp.readValuesAs(String.class);
+						names = jp.readValuesAs(String.class);
+						types = jp.readValuesAs(String.class);
+						physics = jp.readValuesAs(String.class);
+						autos = jp.readValuesAs(String.class);
+						while (names.hasNext()){
+							pc = PlatformConfig.builder().setID(ID.next())
+									.setScreenName(names.next())
+									.setType(types.next())
+									.setPhysicsModel(physics.next())
+									.setAutonomousModel(autos.next())
+									.build();
+							platforms.add(pc);
+							ID.remove();
+							names.remove();
+							types.remove();
+							physics.remove();
+							autos.remove();
+						}
+					}
 				}
 			}
-			this.mapFromFile = false;
-			for(int rn=0; rn<NT.roverNames.size(); rn++){
-				RoverPhysicsModel ph = new RoverPhysicsModel();
-				RoverAutonomousCode ac = new RoverAutonomousCode(NT.getRoverNames().get(rn),NT.getRoverTags().get(rn)) {
-					@Override
-					public String nextCommand(long milliTime, DecimalPoint location, double direction, Map<String, Double> parameters) {
-						return null;
-					}
-
-					@Override
-					public RoverAutonomousCode clone() {
-						return null;
-					}
-				};
-				DecimalPoint dp = new DecimalPoint();
-				RoverObject n = new RoverObject(NT.getRoverNames().get(rn), NT.getRoverTags().get(rn), ph, ac, dp, 0,0);
-				this.rovers.add(n);
-			}
-			Platform.buildFromConfiguration(RoverState.defaultState())
-			for(int rn=0; rn<NT.getSatelliteNames().size(); rn++){
-				SatelliteParametersList spl = new SatelliteParametersList();
-				SatelliteAutonomusCode sac = new SatelliteAutonomusCode(NT.getSatelliteNames().get(rn),NT.getSatelliteTags().get(rn)) {
-					@Override
-					public String nextCommand(long milliTime, DecimalPoint location, double direction, double acceleration, double angular_acceleration, double wheel_speed_FL, double wheel_speed_FR, double wheel_speed_BL, double wheel_speed_BR, double motor_current_FL, double motor_current_FR, double motor_current_BL, double motor_current_BR, double motor_temp_FL, double motor_temp_FR, double motor_temp_BL, double motor_temp_BR, double battery_voltage, double battery_current, double battery_temp) {
-						return null;
-					}
-				};
-
-				SatelliteObject so = new SatelliteObject(NT.getSatelliteNames().get(rn),NT.getSatelliteTags().get(rn), spl,sac, 1000,0,0 );
-				this.satellites.add(so);
-			}
-		}
 		jp.close();
 		/*
 		this.mapFromFile = input.mapFromFile;
@@ -199,44 +224,6 @@ public class RunConfiguration implements Serializable {
 		JsonFactory f = new JsonFactory();
 		JsonGenerator g = f.createGenerator(new File("jtest.json"), JsonEncoding.UTF8);
 		g.writeStartObject();
-		g.writeObjectFieldStart("namesAndTags");
-		g.writeFieldName("GroundNames");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getGroundNames().size(); j++){
-			g.writeString(namesAndTags.getNames().get(j));}
-		g.writeEndArray();
-		g.writeFieldName("RoverNames");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getRoverNames().size(); j++){
-			g.writeString(namesAndTags.getRoverNames().get(j));
-		}
-		g.writeEndArray();
-		g.writeFieldName("SatelliteNames");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getSatelliteNames().size(); j++){
-			g.writeString(namesAndTags.getSatelliteNames().get(j));
-		}
-		g.writeEndArray();
-		g.writeFieldName("GroundTags");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getGroundTags().size(); j++){
-			g.writeString(namesAndTags.getGroundTags().get(j));
-		}
-		g.writeEndArray();
-		g.writeFieldName("RoverTags");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getRoverTags().size(); j++){
-			g.writeString(namesAndTags.getRoverTags().get(j));
-		}
-		g.writeEndArray();
-		g.writeFieldName("SatelliteTags");
-		g.writeStartArray();
-		for(int j=0; j<namesAndTags.getSatelliteTags().size(); j++){
-			g.writeString(namesAndTags.getSatelliteTags().get(j));
-		}
-		g.writeEndArray();
-		g.writeEndObject();
-		g.writeStartObject();;
 		g.writeObjectFieldStart("Runtime");
 		g.writeNumberField("Runtime",runtime);
 		g.writeBooleanField("acclelerated", accelerated);
@@ -256,7 +243,39 @@ public class RunConfiguration implements Serializable {
 		g.writeBooleanField("(monoHazards", monoHazards);
 		g.writeNumberField("targetDensity", targetDensity);
 		g.writeNumberField("hazardDensity", hazardDensity);
-
+		g.writeEndObject();
+		g.writeStartObject();;
+		g.writeObjectFieldStart("PlatformConfig");
+		g.writeFieldName("ID");
+		g.writeStartArray();
+		for(int j=0; j<platforms.size(); j++){
+			g.writeString(platforms.get(j).getID());
+		}
+		g.writeEndArray();
+		g.writeFieldName("screenName");
+		g.writeStartArray();
+		for(int j=0; j<platforms.size(); j++){
+			g.writeString(platforms.get(j).getScreenName());
+		}
+		g.writeEndArray();
+		g.writeFieldName("Type");
+		g.writeStartArray();
+		for(int j=0; j<platforms.size(); j++){
+			g.writeString(platforms.get(j).getType());
+		}
+		g.writeEndArray();
+		g.writeFieldName("PhysicsConfig");
+		g.writeStartArray();
+		for(int j=0; j<platforms.size(); j++){
+			g.writeString(platforms.get(j).getPhysicsModelName());
+		}
+		g.writeEndArray();
+		g.writeFieldName("AutonomousConfig");
+		g.writeStartArray();
+		for(int j=0; j<platforms.size(); j++){
+			g.writeString(platforms.get(j).getAutonomousModelName());
+		}
+		g.writeEndArray();
 		g.writeEndObject();
 
 		g.close();
