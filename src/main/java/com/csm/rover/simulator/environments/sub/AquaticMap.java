5b35c67577b1858130e6b5027442cc4d794b2c5d
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2016-09-08 23:35:23 -0600

    Begin Sub Environment Development
    Merged in Environments and began implementation for subs

--- src/main/java/com/csm/rover/simulator/environments/sub/AquaticMap.java
+++ src/main/java/com/csm/rover/simulator/environments/sub/AquaticMap.java
@@ -32,25 +32,25 @@ public class AquaticMap extends EnvironmentMap {
         this.detail = detail;
     }
 
-    public AquaticMap(){
+    public AquaticMap() {
         this(0, 0);
     }
 
     @JsonCreator
-    public AquaticMap(@JsonProperty("size") int size, @JsonProperty("detail") int detail, @JsonProperty("heightMap") ArrayGrid<Float> values){
+    public AquaticMap(@JsonProperty("size") int size, @JsonProperty("detail") int detail, @JsonProperty("heightMap") ArrayGrid<Float> values) {
         this(size, detail);
-        this.heightMap = new FloatArrayArrayGrid((FloatArrayArrayGrid)values);
+        this.heightMap = new FloatArrayArrayGrid((FloatArrayArrayGrid) values);
         checkSize();
     }
 
-    public AquaticMap(int size, int detail, Float[][] values){
+    public AquaticMap(int size, int detail, Float[][] values) {
         this(size, detail);
         this.heightMap = new FloatArrayArrayGrid(values);
         checkSize();
     }
 
-    private void checkSize(){
-        if (heightMap.getWidth() != size*detail || heightMap.getHeight() != size*detail){
+    private void checkSize() {
+        if (heightMap.getWidth() != size * detail || heightMap.getHeight() != size * detail) {
             throw new IllegalArgumentException("The map does not match the given sizes");
         }
     }
@@ -60,16 +60,16 @@ public class AquaticMap extends EnvironmentMap {
     @JsonIgnore
     private Optional<Float> min_val = Optional.empty();
 
-    private void findMaxMin(){
+    private void findMaxMin() {
         float max = Float.MIN_VALUE;
         float min = Float.MAX_VALUE;
-        for (int i = 0; i < heightMap.getWidth(); i++){
-            for (int j = 0; j < heightMap.getHeight(); j++){
+        for (int i = 0; i < heightMap.getWidth(); i++) {
+            for (int j = 0; j < heightMap.getHeight(); j++) {
                 float val = heightMap.get(i, j);
-                if (val > max){
+                if (val > max) {
                     max = val;
                 }
-                if (val < min){
+                if (val < min) {
                     min = val;
                 }
             }
@@ -79,67 +79,66 @@ public class AquaticMap extends EnvironmentMap {
     }
 
     @JsonIgnore
-    public float getMaxValue(){
-        if (!max_val.isPresent()){
+    public float getMaxValue() {
+        if (!max_val.isPresent()) {
             findMaxMin();
         }
         return max_val.get();
     }
 
     @JsonIgnore
-    public float getMinValue(){
-        if (!min_val.isPresent()){
+    public float getMinValue() {
+        if (!min_val.isPresent()) {
             findMaxMin();
         }
         return min_val.get();
     }
 
-    private Point getMapSquare(DecimalPoint loc){ // says which display square a given coordinate falls in
+    private Point getMapSquare(DecimalPoint loc) { // says which display square a given coordinate falls in
         int shift = heightMap.getWidth() / (detail * 2);
         double x = loc.getX() + shift;
         double y = shift - loc.getY();
-        int outx = (int)(x*detail);
-        int outy = (int)(y*detail);
+        int outx = (int) (x * detail);
+        int outy = (int) (y * detail);
         return new Point(outx, outy);
     }
 
-    private Point getGridSquare(DecimalPoint loc){
+    private Point getGridSquare(DecimalPoint loc) {
         Point square = getMapSquare(loc);
-        return new Point(square.x/3, square.y/3);
+        return new Point(square.x / 3, square.y / 3);
     }
 
     //returns the height of the map at the given point
-    public double getHeightAt(DecimalPoint loc){
+    public double getHeightAt(DecimalPoint loc) {
         Point mapSquare = getMapSquare(loc);
         int x = (int) mapSquare.getX();
         int y = (int) mapSquare.getY();
-        DecimalPoint lifePnt = new DecimalPoint(loc.getX() + getSize()/2.0, getSize()/2.0 - loc.getY());
-        double locx = ((int)((lifePnt.getX() - (int)lifePnt.getX())*1000) % (1000/detail)) / 1000.0 * detail;
-        double locy = ((int)((lifePnt.getY() - (int)lifePnt.getY())*1000) % (1000/detail)) / 1000.0 * detail;
+        DecimalPoint lifePnt = new DecimalPoint(loc.getX() + getSize() / 2.0, getSize() / 2.0 - loc.getY());
+        double locx = ((int) ((lifePnt.getX() - (int) lifePnt.getX()) * 1000) % (1000 / detail)) / 1000.0 * detail;
+        double locy = ((int) ((lifePnt.getY() - (int) lifePnt.getY()) * 1000) % (1000 / detail)) / 1000.0 * detail;
         return getIntermediateValue(heightMap.get(x, y), heightMap.get(x + 1, y), heightMap.get(x, y + 1), heightMap.get(x + 1, y + 1), locx, locy);
     }
 
-    private double getIntermediateValue(double topleft, double topright, double bottomleft, double bottomright, double relativex, double relativey){ //find the linear approximation of a value within a square where relative x and y are measured fro mtop left
-        if (relativex > relativey){ //top right triangle
+    private double getIntermediateValue(double topleft, double topright, double bottomleft, double bottomright, double relativex, double relativey) { //find the linear approximation of a value within a square where relative x and y are measured fro mtop left
+        if (relativex > relativey) { //top right triangle
             return (topright - topleft) * relativex - (topright - bottomright) * relativey + topleft;
-        }
-        else if (relativex < relativey){ //bottom left triangle
+        } else if (relativex < relativey) { //bottom left triangle
             return (bottomright - bottomleft) * relativex + (bottomleft - topleft) * relativey + topleft;
-        }
-        else { //center line
+        } else { //center line
             return ((bottomright - topleft) * relativex + topleft);
         }
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public int getDetail(){
+    public int getDetail() {
         return detail;
     }
 
-    public ArrayGrid<Float> rawValues(){
+    public ArrayGrid<Float> rawValues() {
         return heightMap.clone();
     }
+}
 
