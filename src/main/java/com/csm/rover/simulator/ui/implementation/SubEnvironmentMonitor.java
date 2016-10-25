dc7d955d72c64f3d1f3ce1c74eef543f12bba428
CSM-PHM-Lab <CSM.PHM.Lab@gmail.com>
2016-10-24 22:05:47 -0600

    Add new platform and environment arcitecture
    new files extended new abstract displays

---
+++ src/main/java/com/csm/rover/simulator/ui/implementation/SubEnvironmentMonitor.java
@@ -0,0 +1,18 @@
+package com.csm.rover.simulator.ui.implementation;
+
+import com.csm.rover.simulator.environments.PlatformEnvironment;
+
+@FrameMarker(name = "Sub Platform Monitor", platform = "Sub")
+public class SubEnvironmentMonitor extends EnvironmentDisplay{
+
+
+    @Override
+    protected void doSetEnvironment(PlatformEnvironment environment) {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+}
